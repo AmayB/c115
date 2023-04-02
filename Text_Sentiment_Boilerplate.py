35c16e7d26baedd72c302f62f18df50d50d1533f
AmayB <87885769+AmayB@users.noreply.github.com>
2023-04-02 09:58:58 -0700

    Add files via upload

---
+++ Text_Sentiment_Boilerplate.py
@@ -0,0 +1,31 @@
+import tensorflow.keras
+import numpy as np
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+sentence = ["I am happy to meet my friends. We are planning to go a party.", 
+            "I had a bad day at school. i got hurt while playing football"]
+
+tokenizer = Tokenizer(num_words=10000, oov_token='<OOV>')
+tokenizer.fit_on_texts(sentence)
+
+word_index = tokenizer.word_index
+
+sequence = tokenizer.texts_to_sequences(sentence)
+
+print(sequence[0:2])
+
+padded = pad_sequences(sequence, maxlen=100,
+                       padding='post',truncating='post')
+
+print(padded[0:2])
+
+model=tensorflow.keras.models.load_model('Text_Emotion.h5')
+
+result=model.predict(padded)
+
+print(result)
+
+predict_class = np.argmax(result, axis=1)
+
+print(predict_class)
